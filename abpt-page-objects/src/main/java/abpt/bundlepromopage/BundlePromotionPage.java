395275f1863bc23a2b3441a4f9b16b6bed9d23bc
Luke DeRienzo <javasoftwarearchitect@gmail.com>
2020-06-09 19:30:40 -0400

    initial commit - base functionality, just loading bundle promo page and closing.

---
+++ abpt-page-objects/src/main/java/abpt/bundlepromopage/BundlePromotionPage.java
@@ -0,0 +1,16 @@
+package abpt.bundlepromopage;
+
+import org.openqa.selenium.WebDriver;
+
+import abpt.framework.core.BasePage;
+
+public class BundlePromotionPage extends BasePage {
+
+    public BundlePromotionPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // dd functionality to open bundle promo page, and select and add bundles to the cart
+
+
+}
---
+++ abpt-regression-tests/src/main/java/abpt/tests/bundlepromopage/BundlePromotionPageTests.java
@@ -0,0 +1,16 @@
+package abpt.tests.bundlepromopage;
+
+import org.testng.annotations.Test;
+
+import abpt.bundlepromopage.BundlePromotionPage;
+import abpt.framework.core.BaseTest;
+
+public class BundlePromotionPageTests extends BaseTest {
+    private static final String BUNDLE_PROMO_PAGE_URL = "http://localhost:8000/";
+
+    @Test
+    public void loadBundlePromoPageTest() {
+        BundlePromotionPage bundlePromotionPage = new BundlePromotionPage(getDriver());
+        bundlePromotionPage.loadPage(BUNDLE_PROMO_PAGE_URL);
+    }
+}
