ccf0da2c3bdee293895b0c15fcd1b01bd1af693b
Luke DeRienzo <javasoftwarearchitect@gmail.com>
2020-06-10 08:43:48 -0400

    expansion of tests to encompas more test cases. additon of empty cart functionality(but not working right).

--- abpt-page-objects/src/main/java/abpt/bundlepromopage/BundlePromotionPage.java
+++ abpt-page-objects/src/main/java/abpt/bundlepromopage/BundlePromotionPage.java
@@ -1,5 +1,7 @@
 package abpt.bundlepromopage;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import abpt.framework.core.BasePage;
 
 public class BundlePromotionPage extends BasePage {
+    private static final String EMPTY_CART_ID = "empty_cart";
 
     public BundlePromotionPage(WebDriver driver) {
         super(driver);
@@ -37,8 +40,44 @@ public class BundlePromotionPage extends BasePage {
     }
 
     public void goBackToPromotionPageFromShoppingCartPage() {
-        WebElement backButton = driver.findElement(By.id("back"));
+        WebElement backButton = findElementById("back");
         backButton.click();
+    }
+
+    private WebElement findElementById(String id) {
+        return driver.findElement(By.id(id));
+    }
+
+    public void emptyShoppingCart() {
+        waitForElementToAppear(By.id(EMPTY_CART_ID));
+        WebElement emptyCart = findElementById(EMPTY_CART_ID);
+        emptyCart.click();
+    }
+
+    public void pauseForSeconds(int secondsToPause) {
+        driver.manage().timeouts().implicitlyWait(secondsToPause, TimeUnit.SECONDS);
+    }
+
+    public int getNumberOfThreeMonthBundlesInCart() {
+        return Integer.valueOf(getQuantityOfCartItem("3"));
+    }
+
+    private String getQuantityOfCartItem(String monthSupply) {
+        WebElement tdContainingQuantity = findElementById(monthSupply);
+        return tdContainingQuantity.getText();
+    }
+
+    public int getNumberOfSixMonthBundlesInCart() {
+        return Integer.valueOf(getQuantityOfCartItem("6"));
+    }
+
+    public int getNumberOfTwelveMonthBundlesInCart() {
+        return Integer.valueOf(getQuantityOfCartItem("12"));
+    }
+
+    public int getCartTotal() {
+        WebElement tdContainingQuantity = findElementById("total");
+        return Integer.parseInt(tdContainingQuantity.getText());
     }
 
 }
--- abpt-regression-tests/src/main/java/abpt/tests/bundlepromopage/BundlePromotionPageTests.java
+++ abpt-regression-tests/src/main/java/abpt/tests/bundlepromopage/BundlePromotionPageTests.java
@@ -1,5 +1,10 @@
 package abpt.tests.bundlepromopage;
 
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import abpt.bundlepromopage.BundlePromotionPage;
@@ -7,18 +12,91 @@ import abpt.framework.core.BaseTest;
 
 public class BundlePromotionPageTests extends BaseTest {
     private static final String BUNDLE_PROMO_PAGE_URL = "http://localhost:8000/";
+    private BundlePromotionPage bundlePromotionPage;
+
+
+    @BeforeTest
+    public void setUp() {
+        bundlePromotionPage = new BundlePromotionPage(getDriver());
+        bundlePromotionPage.loadPage(BUNDLE_PROMO_PAGE_URL);
+    }
+
+    public void clearDb() {
+        bundlePromotionPage.emptyShoppingCart();
+    }
 
     @Test
-    public void loadBundlePromoPageTest() {
-        BundlePromotionPage bundlePromotionPage = new BundlePromotionPage(getDriver());
-        bundlePromotionPage.loadPage(BUNDLE_PROMO_PAGE_URL);
+    public void testAddingOfThreeMonthBundle() {
+        // given/when
+        bundlePromotionPage.addThreeMonthSupplyBundleToCart();
+        int quantityOfThreeMonthSupply = bundlePromotionPage.getNumberOfThreeMonthBundlesInCart();
+        int orderTotal = bundlePromotionPage.getCartTotal();
+        clearDb();
+        // then
+        assertEquals(quantityOfThreeMonthSupply, 1);
+        assertEquals(orderTotal, 244);
+    }
 
+    @Test
+    public void testAddingOfSixMonthBundle() {
+        // given/when
+        bundlePromotionPage.addSixMonthSupplyBundleToCart();
+        int quantityOfSixMonthSupply = bundlePromotionPage.getNumberOfSixMonthBundlesInCart();
+        int orderTotal = bundlePromotionPage.getCartTotal();
+        clearDb();
+        // then
+        assertEquals(quantityOfSixMonthSupply, 1);
+        assertEquals(orderTotal, 396);
+    }
+
+    @Test
+    public void testAddingOfTwelveMonthBundle() {
+        // given/when
+        bundlePromotionPage.addTwelveMonthSupplyBundleToCart();
+        int quantityOfTwelveMonthSupply = bundlePromotionPage.getNumberOfTwelveMonthBundlesInCart();
+        int orderTotal = bundlePromotionPage.getCartTotal();
+        clearDb();
+        // then
+        assertEquals(quantityOfTwelveMonthSupply, 1);
+        assertEquals(orderTotal, 768);
+    }
+
+
+    @Test
+    public void testAddingMultipleThreeMonthBundles() {
+        // given/when
         bundlePromotionPage.addThreeMonthSupplyBundleToCart();
         bundlePromotionPage.goBackToPromotionPageFromShoppingCartPage();
+        bundlePromotionPage.addThreeMonthSupplyBundleToCart();
+        int quantityOfThreeMonthSupply = bundlePromotionPage.getNumberOfThreeMonthBundlesInCart();
+        int orderTotal = bundlePromotionPage.getCartTotal();
+        clearDb();
+        // then
+        assertEquals(quantityOfThreeMonthSupply, 2);
+        assertEquals(orderTotal, 488);
+    }
 
-        bundlePromotionPage.addSixMonthSupplyBundleToCart();
+
+    @Test
+    public void testAddingOneOfEachBundle() {
+        // given
+        bundlePromotionPage.addThreeMonthSupplyBundleToCart();
+        bundlePromotionPage.pauseForSeconds(1);
         bundlePromotionPage.goBackToPromotionPageFromShoppingCartPage();
-
+        bundlePromotionPage.pauseForSeconds(1);
+        bundlePromotionPage.addSixMonthSupplyBundleToCart();
+        bundlePromotionPage.pauseForSeconds(1);
+        bundlePromotionPage.goBackToPromotionPageFromShoppingCartPage();
+        bundlePromotionPage.pauseForSeconds(1);
         bundlePromotionPage.addTwelveMonthSupplyBundleToCart();
+        bundlePromotionPage.pauseForSeconds(1);
+        // when
+        int quantityOfThreeMonthSupply = bundlePromotionPage.getNumberOfThreeMonthBundlesInCart();
+
+        // then
+        assertEquals(quantityOfThreeMonthSupply, 1);
+
+        bundlePromotionPage.pauseForSeconds(1);
+        clearDb();
     }
 }
